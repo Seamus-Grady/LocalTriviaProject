cdb4a6cfe113addea9dd16c8bedc832f6a1664a4
Seamus-Grady <u0995999@utah.edu>
2020-12-23 03:24:51 -0600

    Created the representation of the board for this game

--- LocalTriviaProject/LocalTriviaProject/BoardNode.cs
+++ LocalTriviaProject/LocalTriviaProject/BoardNode.cs
@@ -8,13 +8,25 @@ namespace LocalTriviaProject
 {
     public class BoardNode
     {
+        public BoardNode(int type)
+        {
+            Type = type;
+        }
         public BoardNode left { get; set; }
         public BoardNode right { get; set; }
         public BoardNode straight { get; set; }
+        public BoardNode backwards { get; set; }
         public int Category { get; set; }
+        public int Type { get; set; }
+
+        public int myType()
+        {
+            return Type;
+        }
     }
     public class CenterNode : BoardNode
     {
+        public CenterNode() : base (1) {}
         public BoardNode YellowPath { get; set; }
         public BoardNode GreenPath { get; set; }
         public BoardNode PinkPath { get; set; }
--- LocalTriviaProject/LocalTriviaProject/Program.cs
+++ LocalTriviaProject/LocalTriviaProject/Program.cs
@@ -9,13 +9,16 @@ namespace LocalTriviaProject
     class Program
     {
         static Dictionary<int, BoardNode> board;
-        static BoardNode purpleStart;
-        static BoardNode orangeStart;
-        static BoardNode greenStart;
-        static BoardNode pinkStart;
+        static int purpleStart;
+        static int orangeStart;
+        static int greenStart;
+        static int pinkStart;
+        static int blueStart;
+        static int yellowStart;
         static void Main(string[] args)
         {
             int color;
+            CreateBoard();
             Player currentPlayer = new Player();
             Console.Write("Hello Welcome to Trivia Pursuit please enter a username: ");
             currentPlayer.userName =  Console.ReadLine();
@@ -33,10 +36,290 @@ namespace LocalTriviaProject
             Console.ReadLine();
         }
 
-        private void CreateBoard()
+        private static void CreateBoard()
         {
+            int count = 0;
             board = new Dictionary<int, BoardNode>();
+            board.Add(0, new CenterNode());
+            CreateBluePath(ref count);
+            CreatePinkPath(ref count);
+            CreateYellowPath(ref count);
+            CreatePurplePath(ref count);
+            CreateGreenPath(ref count);
+            CreateOrangePath(ref count);
+            CreateRestofBoard(ref count);
 
+        }
+        private static void CreateBluePath(ref int count)
+        {
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[0], Category = 3 });
+            CenterNode n = (CenterNode)board[0];
+            n.BluePath = board[count];
+            board[count].backwards = board[0];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count-1], Category = 5 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 2 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 4 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 1 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 0 });
+            board[count - 1].backwards = board[count];
+            blueStart = count;
+        }
+
+        private static void CreatePinkPath(ref int count)
+        {
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[0], Category = 2 });
+            CenterNode n = (CenterNode)board[0];
+            n.PinkPath = board[count];
+            board[count].backwards = board[0];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 4 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 3 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 5 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 0 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 2 });
+            board[count - 1].backwards = board[count];
+            pinkStart = count;
+        }
+
+        private static void CreateYellowPath(ref int count)
+        {
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[0], Category = 5 });
+            CenterNode n = (CenterNode)board[0];
+            n.YellowPath = board[count];
+            board[count].backwards = board[0];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 1 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 4 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 0 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 3 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 2 });
+            board[count - 1].backwards = board[count];
+            yellowStart = count;
+        }
+
+        private static void CreatePurplePath(ref int count)
+        {
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[0], Category = 4 });
+            CenterNode n = (CenterNode)board[0];
+            n.PurplePath = board[count];
+            board[count].backwards = board[0];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 0 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 5 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 1 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 2 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 3 });
+            board[count - 1].backwards = board[count];
+            purpleStart = count;
+        }
+
+        private static void CreateGreenPath(ref int count)
+        {
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[0], Category = 1 });
+            CenterNode n = (CenterNode)board[0];
+            n.GreenPath = board[count];
+            board[count].backwards = board[0];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 3 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 0 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 2 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 5 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 4 });
+            board[count - 1].backwards = board[count];
+            greenStart = count;
+        }
+
+        private static void CreateOrangePath(ref int count)
+        {
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[0], Category = 0 });
+            CenterNode n = (CenterNode)board[0];
+            n.OrangePath = board[count];
+            board[count].backwards = board[0];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 2 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 1 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 3 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 4 });
+            board[count - 1].backwards = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { straight = board[count - 1], Category = 5 });
+            board[count - 1].backwards = board[count];
+            orangeStart = count;
+        }
+        private static void CreateRestofBoard(ref int count)
+        {
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[blueStart], Category = 1 });
+            board[blueStart].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count -1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 4 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 5 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], right = board[purpleStart], Category = 2 });
+            board[count - 1].right = board[count];
+
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[purpleStart], Category = 2 });
+            board[purpleStart].right = board[count];
+            board[purpleStart].left = board[count - 1];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 1 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 0 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], right = board[greenStart], Category = 5 });
+            board[count - 1].right = board[count];
+
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[greenStart], Category = 5 });
+            board[greenStart].right = board[count];
+            board[greenStart].left = board[count - 1];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 2 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 3 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], right = board[pinkStart], Category = 0 });
+            board[count - 1].right = board[count];
+
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[pinkStart], Category = 0 });
+            board[pinkStart].right = board[count];
+            board[pinkStart].left = board[count - 1];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 5 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 4 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], right = board[yellowStart], Category = 3 });
+            board[count - 1].right = board[count];
+
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[yellowStart], Category = 3 });
+            board[yellowStart].right = board[count];
+            board[yellowStart].left = board[count - 1];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 0 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 1 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], right = board[orangeStart], Category = 4 });
+            board[count - 1].right = board[count];
+            
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[orangeStart], Category = 4 });
+            board[orangeStart].right = board[count];
+            board[orangeStart].left = board[count - 1];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 3 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 2 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 6 });
+            board[count - 1].right = board[count];
+            count++;
+            board.Add(count, new BoardNode(0) { left = board[count - 1], Category = 1 });
+            board[count - 1].right = board[count];
+            board[blueStart].left = board[count];
         }
     }
 }
